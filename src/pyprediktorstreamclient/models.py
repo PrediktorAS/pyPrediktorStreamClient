9452440a499abf190c86c1145a6a40c21708d495
Andreas Lydersen <andreas.lydersen@prediktor.com>
2022-11-08 09:36:38 +0100

    Added basic verification

--- src/pyprediktorstreamclient/models.py
+++ src/pyprediktorstreamclient/models.py
@@ -1,2 +1,39 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Optional
 
+
+class VQT2Payload(BaseModel):
+    k: str
+    v: float
+    q: Optional[int]
+    t: Optional[datetime]
+
+class VQT2(BaseModel):
+    messagetype: str
+    version: int
+    payload: List[VQTPayload]
+    
+
+test = {
+    "messagetype":"vqt",
+    "version":2,
+    "payload":
+    [
+        {
+            "k":"tag1",
+            "v":1.0
+        },
+        {
+            "k":"tag1",
+            "v":0.9,
+            "t":"2022-09-25T20:00:00.000Z"
+        },
+        {
+            "k":"tag1",
+            "v":0.8,
+            "q":192,
+            "t":"2022-09-25T19:00:00.000Z"
+        }
+    ]
+}
