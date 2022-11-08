94b48d2714a074b58c265d2d1e35d5c5f62dd49c
Andreas Lydersen <andreas.lydersen@prediktor.com>
2022-11-08 10:49:11 +0100

    Adding basic test

--- AUTHORS.rst
+++ AUTHORS.rst
@@ -2,4 +2,5 @@
 Contributors
 ============
 
+* Sobyah siddiqui <sobyah.siddiqui@prediktor.com>
 * Andreas Lydersen <andreas.lydersen@prediktor.com>
--- src/pyprediktorstreamclient/models.py
+++ src/pyprediktorstreamclient/models.py
@@ -12,28 +12,4 @@ class VQT2Payload(BaseModel):
 class VQT2(BaseModel):
     messagetype: str
     version: int
-    payload: List[VQTPayload]
-    
-
-test = {
-    "messagetype":"vqt",
-    "version":2,
-    "payload":
-    [
-        {
-            "k":"tag1",
-            "v":1.0
-        },
-        {
-            "k":"tag1",
-            "v":0.9,
-            "t":"2022-09-25T20:00:00.000Z"
-        },
-        {
-            "k":"tag1",
-            "v":0.8,
-            "q":192,
-            "t":"2022-09-25T19:00:00.000Z"
-        }
-    ]
-}
+    payload: List[VQT2Payload]
