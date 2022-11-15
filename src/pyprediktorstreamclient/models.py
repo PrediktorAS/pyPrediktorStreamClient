68017fe64ef7a31332b3179ce024f4958a5a457d
SobyahSiddiqui <Sobyah.Siddiqui@prediktor.com>
2022-11-15 17:03:03 +0500

    added meta message class

--- src/pyprediktorstreamclient/models.py
+++ src/pyprediktorstreamclient/models.py
@@ -1,6 +1,6 @@
 import pydantic
 from datetime import datetime, timedelta
-from typing import List, Optional, ClassVar
+from typing import List, Optional
 import re
 import pytz
 
@@ -90,16 +90,15 @@ class EventPayload(pydantic.BaseModel):
     activetime: Optional[datetime]
     lastquality: Optional[int]
 
-
     check_time = pydantic.validator("time", allow_reuse=True)(validating_timestamp)
     check_activetime = pydantic.validator("activetime", allow_reuse=True)(validating_timestamp)
 
     @pydantic.validator("state")
     @classmethod
     def validating_source(cls, value) -> str:
-        states= [None, 'Enabled', 'Active', 'Acked', 'AckRequired', 'Confirmed',
-                                       'Suppressed', 'Shelved', 'Subsates', 'Low', 'High', 'HighHigh', 'LowLow']
-        states_entered= value.replace(' ','').split(',')
+        states = [None, 'Enabled', 'Active', 'Acked', 'AckRequired', 'Confirmed',
+                  'Suppressed', 'Shelved', 'Subsates', 'Low', 'High', 'HighHigh', 'LowLow']
+        states_entered = value.replace(' ', '').split(',')
         for state in states_entered:
             if state not in states:
                 raise ValueError('Invalid event state entered')
@@ -121,5 +120,24 @@ class Event(pydantic.BaseModel):
             raise ValueError('messagetype field should be "events" to write to events to the server')
         return value
 
+
+class MetaPayload(pydantic.BaseModel):
+    k: str
+
+    class Config:
+        extra = pydantic.Extra.allow
+
+
+class Meta(pydantic.BaseModel):
+    messagetype: str
+    version: int
+    payload: List[MetaPayload]
+
+    @pydantic.validator('messagetype')
+    @classmethod
+    def validating_messagetype(cls, value) -> str:
+        if value != 'meta':
+            raise ValueError('messagetype field should be "meta" to write to events to the server')
+        return value
 
 
